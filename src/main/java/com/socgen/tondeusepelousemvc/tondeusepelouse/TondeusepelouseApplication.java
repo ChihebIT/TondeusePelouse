ef1c8f2c6cc45e6e6a8a3fc43cd66a33f4c6fae8
chiheb.works <chiheb.works@gmail.com>
2024-06-06 01:37:45 +0200

    Initial commit

---
+++ src/main/java/com/socgen/tondeusepelousemvc/tondeusepelouse/TondeusepelouseApplication.java
@@ -0,0 +1,13 @@
+package com.socgen.tondeusepelousemvc.tondeusepelouse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TondeusepelouseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TondeusepelouseApplication.class, args);
+	}
+
+}
